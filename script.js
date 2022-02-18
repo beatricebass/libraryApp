bc2d6ba78c0327b77d494b2c0120ddcb4042c321
Beatrice <beatricebass94@gmail.com>
2022-02-18 15:28:06 -0500

    add addToLibrary () and input fields

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function Book(title, author, pages, read) {
+function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.isRead = isRead;
   this.info = function () {
-    let status = (read === true) ? "read" : "not read yet";
-    return (title + " by " + author + ", " + pages + " pages, " + status);
+    let read = (isRead === true) ? "read" : "not read yet";
+    return (title + " by " + author + ", " + pages + " pages, " + read);
   }
 }
 
@@ -13,8 +13,25 @@ const book1 = new Book ("Pride and Prejudice", "Jane Austen", "432", true);
 const book2 = new Book ("The Eye of The World", "Robert Jordan", "782", true);
 const book3 = new Book ("The Pragmatic Programmer", "Andrew Hunt", "320", false);
 
+let myLibrary = [book1, book2, book3];
+
+let title;
+let author;
+let pages;
+let isRead;
+
 function addBookToLibrary () {
-  
+  title = document.querySelector(".title").value;
+  author = document.querySelector(".author").value;
+  pages = document.querySelector(".pages").value;
+  isRead = document.querySelector(".isRead").checked;
+
+  let newBook = new Book (title, author, pages, isReadmy);
+
+  myLibrary.push(newBook);
 }
 
-let myLibrary = [book1, book2, book3];
+function getInformation () {
+
+}
+
