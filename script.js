ecfa15b217edf53c56b0f637e75420d89c6e23d1
Beatrice <beatricebass94@gmail.com>
2022-02-19 13:46:34 -0500

    functionality to add new book button

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const book1 = new Book ("Pride and Prejudice", "Jane Austen", "432", true);
+const book2 = new Book ("The Eye of The World", "Robert Jordan", "782", true);
+const book3 = new Book ("The Pragmatic Programmer", "Andrew Hunt", "320", false);
 
+let myLibrary = [book1, book2, book3];
+
+const addBook = document.querySelector(".addBook");
+console.log(addBook);
+addBook.addEventListener('click', createInputCard);
+
+//this is the Book constructor:makes books into obejcts and added to array when used in conjunction with addBooksToLibrary()
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
@@ -6,41 +16,72 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead;
 }
 
-const library = document.querySelector(".library");
-
+//prototype to retrieve information about the books in order to display on screen
 Book.prototype.info = function () {
   let card = document.createElement("div");
   card.setAttribute("class", "card");
-  let title = document.createElement("p");
-  let author = document.createElement("p");
-  let pages = document.createElement("p");
-  let isRead = document.createElement("input")
+  let cardTitle = document.createElement("p");
+  let cardAuthor = document.createElement("p");
+  let cardPages = document.createElement("p");
+  let cardIsRead = document.createElement("input")
   isRead.setAttribute("type", "checkbox")
 
-  title.textContent = "Title: " + this.title;
-  author.textContent = "Author: " + this.author;
-  pages.textContent = "Page Count: " + this.pages;
-  this.isRead === true ? isRead.checked = true : isRead.checked = false
-  card.appendChild(title);
-  card.appendChild(author);
-  card.appendChild(pages)
-  card.appendChild(isRead);
+  cardTitle.textContent = "Title: " + this.title;
+  cardAuthor.textContent = "Author: " + this.author;
+  cardPages.textContent = "Page Count: " + this.pages;
+  this.isRead === true ? cardIsRead.checked = true : cardIsRead.checked = false
+  card.appendChild(cardTitle);
+  card.appendChild(cardAuthor);
+  card.appendChild(cardPages)
+  card.appendChild(cardIsRead);
   library.appendChild(card);
 }
 
-const book1 = new Book ("Pride and Prejudice", "Jane Austen", "432", true);
-const book2 = new Book ("The Eye of The World", "Robert Jordan", "782", true);
-const book3 = new Book ("The Pragmatic Programmer", "Andrew Hunt", "320", false);
+function createInputCard() {
+  console.log("Hi");
+  const input = document.querySelector(".header");
+  let card = document.createElement("div");
+  card.setAttribute("class", "card");
+  let header = document.createElement("h3");
+  header.textContent = "Add Book"
+  let title = document.createElement("input");
+  title.setAttribute("class", "title")
+  title.setAttribute("type", "text")
+  title.setAttribute("placeholder", "Title")
+  let author = document.createElement("input");
+  author.setAttribute("class", "author")
+  author.setAttribute("type", "text")
+  author.setAttribute("placeholder", "Author")
+  let pages = document.createElement("input");
+  pages.setAttribute("class", "pages");
+  pages.setAttribute("type", "number")
+  pages.setAttribute("placeholder", "Number of Pages")
+  let status = document.createElement("p");
+  status.setAttribute("class", "status");
+  status.textContent = "Have you read this book yet?"
+  let isRead = document.createElement("input");
+  isRead.setAttribute("class", "isRead");
+  isRead.setAttribute("type", "checkbox");
+  let submitBtn = document.createElement("button");
+  submitBtn.setAttribute("type", "button");
+  submitBtn.setAttribute("class", "addBook");
+  submitBtn.textContent = "Add Book";
 
-let myLibrary = [book1, book2, book3];
-
-let title;
-let author;
-let pages;
-let isRead;
+  card.appendChild(header);
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(status);
+  card.appendChild(isRead);
+  card.appendChild(submitBtn);
+  input.appendChild(card);
+}
 
 function addBookToLibrary () {
-  title = document.querySelector(".title").value;
+  createCard();
+  console.log("hi");
+  title.value;    
+  //  = document.querySelector(".title")
   author = document.querySelector(".author").value;
   pages = document.querySelector(".pages").value;
   isRead = document.querySelector(".isRead").checked;
